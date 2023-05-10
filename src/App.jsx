c2176ac30737231ffe93f3c5c19752b908ddd742
zeeshansyed695 <66292478+zeeshansyed695@users.noreply.github.com>
2023-05-10 12:40:07 +0500

    cleaning

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Myheading from './components/Myheading';
+
+
+function App() {
+  return (
+    
+    <div >
+      <h1>how are you</h1>
+      <Myheading/>
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Myheading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Myheading = () => {
+  return (
+    <div>Myheading</div>
+  )
+}
+
+export default Myheading;
