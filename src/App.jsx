514a3890ad2633590a57b4d73e27d649dfa59bd9
zeeshansyed695 <66292478+zeeshansyed695@users.noreply.github.com>
2023-05-16 19:51:15 +0500

    new one

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,17 @@
-import { useEffect, useState } from "react"
-import { NewTodoForm } from "./components/NewTodoForm"
-import "./components/styles.css"
-import { TodoList } from "./components/TodoList"
+import React, { useState } from 'react'
 
-export default function App() {
-  const [todos, setTodos] = useState(() => {
-    const localValue = localStorage.getItem("ITEMS")
-    if (localValue == null) return []
-
-    return JSON.parse(localValue)
+const App = () => {
+  const [object,setobject] = useState({
+    name: 'zeeshan',
+    age : '31',
+    passion : 'get rich for people'
   })
+  const changeobj = () =>{
+    setobject([]);
+  }
+  
 
-  useEffect(() => {
-    localStorage.setItem("ITEMS", JSON.stringify(todos))
-  }, [todos])
-
-  function addTodo(title) {
-    setTodos(currentTodos => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title, completed: false },
-      ]
-    })
+  
   }
 
-  function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, completed }
-        }
-
-        return todo
-      })
-    })
-  }
-
-  function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id)
-    })
-  }
-
-  return (
-    <>
-      <NewTodoForm onSubmit={addTodo} />
-      <h1 className="header">Todo List</h1>
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
-    </>
-  )
-}
+export default App
