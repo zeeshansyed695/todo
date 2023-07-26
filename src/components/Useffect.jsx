77c612d88644f39cc74efdfd4278a65593cfd952
zeeshansyed695 <66292478+zeeshansyed695@users.noreply.github.com>
2023-07-26 19:36:34 +0500

    form

--- src/components/Useffect.jsx
+++ src/components/Useffect.jsx
@@ -1,38 +1,18 @@
-import React,{useState,useEffect} from 'react'
-import './Useffect.css'
+import React, { useState } from 'react';
 
+const InputField = () => {
+  const [value, setValue] = useState('');
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
-const Useffect = () => {
-  const [user,setUser] = useState([]);
-  const getUser = async () => {
-    const response = await fetch('https://api.github.com/users');
-    setUser(await response.json());
-    console.log(response.json)
-  }
- 
-  useEffect(() =>{
-    getUser();
-  },[])
   return (
-    <div className = 'maindiv'>
-    {user.map((currentElement) => {
-      return(
-        <div className="card">
-        <img src={currentElement.avatar_url} ></img>
-        
-          <h4 className='name'>{currentElement.login}</h4>
-          <h4 className='github'>{currentElement.html_url}</h4>
-          
-        
-        </div>
-
-      )
-    })}
-   
-    
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>Value: {value}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Useffect
+export default InputField
