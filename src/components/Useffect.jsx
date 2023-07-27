c6537cab5e672a18619b55953289e20dfcc05f3d
zeeshansyed695 <66292478+zeeshansyed695@users.noreply.github.com>
2023-07-27 21:34:57 +0500

    jsx

--- src/components/Useffect.jsx
+++ src/components/Useffect.jsx
@@ -16,3 +16,21 @@ const InputField = () => {
 };
 
 export default InputField
+import React, { useState } from 'react';
+
+const InputField = () => {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={value} onChange={handleChange} />
+      <p>Value: {value}</p>
+    </div>
+  );
+};
+
+export default InputField
